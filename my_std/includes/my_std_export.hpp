daee9d5e49b061b50bf5770262fe26760bf71b22
Julien Edmond <julien.edmond@kelenntech.com>
2016-03-07 10:32:01 +0100

    Fixed export/import tag when direct linking

--- my_std/includes/my_std_export.hpp
+++ my_std/includes/my_std_export.hpp
@@ -4,6 +4,8 @@
 
 #ifdef my_std_EXPORTS
 # define MY_STD_EXPORT_TAG SHARED_EXPORT
-#else
+#elif defined my_std_IMPORTS
 # define MY_STD_EXPORT_TAG SHARED_IMPORT
+#else
+# define MY_STD_EXPORT_TAG
 #endif
