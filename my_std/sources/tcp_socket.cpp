7bb6cc82042529633742d9ac50b1ae942394f5b0
U-DESKTOP-HLRF51B\Julien <Julien Edmond>
2016-04-30 16:03:56 +0200

    Added license

--- my_std/sources/tcp_socket.cpp
+++ my_std/sources/tcp_socket.cpp
@@ -17,7 +17,6 @@ freely, subject to the following restrictions:
    misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
 */
-
 #include "tcp_socket.hpp"
 
 my_std::tcp_socket::tcp_socket()
